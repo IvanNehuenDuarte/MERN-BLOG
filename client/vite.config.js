d0df22d5e41c7839103802cdbac86ef4af61fe54
Ivan Duarte <ivan.nehuen.duarte@gmail.com>
2024-06-01 01:15:56 -0300

    Create ui sign-up page

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,5 +8,11 @@ export default defineConfig({
   },
   server: {
     open: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/",
+        secure: false,
+      },
+    },
   },
 });
