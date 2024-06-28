a2b0ea6b9ff3f88c2a111e21c9ce7aa84a2ee2b1
Ivan Duarte <ivan.nehuen.duarte@gmail.com>
2024-06-27 22:37:57 -0300

    Fix navbar and Link error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 import About from "./pages/About";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
