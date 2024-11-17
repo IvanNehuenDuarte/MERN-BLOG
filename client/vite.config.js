35cfc83f18bd86792671444ea35fee7ce13a5b11
Ivan Nehuen Duarte <ivan.nehuen.duarte@gmail.com>
2024-11-17 02:41:32 -0300

    loadenv

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    open: true,
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_BACK_URL,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: "dist",
+    },
+    server: {
+      open: true,
+      proxy: {
+        "/api": {
+          target: env.VITE_BACK_URL || "http://localhost:5000",
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
