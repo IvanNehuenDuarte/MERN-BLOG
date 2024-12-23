6e5aac3ce9991c71be72f935144316b37b781a14
Ivan Nehuen Duarte <ivan.nehuen.duarte@gmail.com>
2024-12-23 17:34:26 -0300

    credential create post

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -90,6 +90,7 @@ export default function CreatePost() {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
+        credentials: "include",
       });
 
       const data = await res.json();
